4831bac23e945d67b8f389128fe13ea13adf60e7
Jacek Hryckiewicz <jacekhry@interia.pl>
2021-10-19 22:04:27 +0200

    Zadania lab 03

--- Lab_03/Assets/Scripts/Zadania/LewoPrawo1_2.cs
+++ Lab_03/Assets/Scripts/Zadania/LewoPrawo1_2.cs
@@ -4,35 +4,27 @@ using UnityEngine;
 
 public class LewoPrawo1_2 : MonoBehaviour
 {
-    public float speed = 2.0f;
+
+    public float speed = 10.0f;
     Rigidbody rb;
 
-    // Start is called before the first frame update
+    
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        rb.AddForce(Vector3.right * speed, ForceMode.Impulse);
     }
 
     void FixedUpdate()
     {
-        // pobranie wartości zmiany pozycji w danej osi
-        // wartości są z zakresu [-1, 1]
-        // kontroler ruchu dla osi jest pobierany z domyślnych ustawień
-        // Input Manager (preferences)
-        //float mH = Input.GetAxis("Horizontal");
-        //float mV = Input.GetAxis("Vertical");
+        if(rb.position.x >= 10)         
+        {
+            rb.AddForce(Vector3.left, ForceMode.Impulse);
+        }
+        if(rb.position.x <= 0)
+        {
+            rb.AddForce(Vector3.right, ForceMode.Impulse);
+        }     
 
-        // tworzymy wektor prędkości
-        Vector3 velocity = new Vector3(1, 0, 0);
-        velocity = velocity.normalized * speed * Time.deltaTime;
-        // wykonujemy przesunięcie Rigidbody z uwzględnieniem sił fizycznych
-        rb.MovePosition(transform.position + velocity);
-    }
-
+    }    
 }
---
+++ Lab_03/Assets/Scripts/Zadania/Zad1_3.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zad1_3 : MonoBehaviour
+{
+    public float speed = 10.0f;
+    Rigidbody rb;
+    Vector3 m_EulerAngleVelocity;
+
+    void Start()
+    {
+        m_EulerAngleVelocity = new Vector3(0, 90, 0);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if(rb.velocity.x == 0 &&  rb.position.x == 0)
+        {
+            rb.AddForce(speed,0,0, ForceMode.VelocityChange);
+            
+        }
+        if(rb.velocity.x == speed && rb.position.x >= 10)
+        {
+            rb.AddForce((-speed),0,(-speed), ForceMode.VelocityChange);
+            Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity);
+            rb.MoveRotation(rb.rotation * deltaRotation);
+        }
+        if(rb.velocity.z == -speed && rb.position.z <= -10)
+        {
+            rb.AddForce(-speed,0,speed, ForceMode.VelocityChange);
+            Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity);
+            rb.MoveRotation(rb.rotation * deltaRotation);
+        }
+        if(rb.velocity.x == -speed && rb.position.x <= 0)
+        {
+            rb.AddForce(speed,0,speed, ForceMode.VelocityChange);
+            Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity);
+            rb.MoveRotation(rb.rotation * deltaRotation);
+        }
+         if(rb.velocity.z == speed && rb.position.z >= 0)
+        {
+            rb.AddForce(speed,0,-speed, ForceMode.VelocityChange);
+            Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity);
+            rb.MoveRotation(rb.rotation * deltaRotation);
+        }
+    }    
+   
+}
+
