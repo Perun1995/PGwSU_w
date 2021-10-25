99c39d1cae4563a81e99959ea018a62b84c8eab6
Jacek Hryckiewicz <jacekhry@interia.pl>
2021-10-25 08:05:59 +0200

    Zadania lab3

---
+++ Zadania/Scripts/lab_3/LewoPrawo2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LewoPrawo1_2 : MonoBehaviour
+{
+
+    public float speed = 10.0f;
+    Rigidbody rb;
+
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.right * speed, ForceMode.Impulse);
+    }
+
+    void FixedUpdate()
+    {
+        if(rb.position.x >= 10)         
+        {
+            rb.AddForce(Vector3.left, ForceMode.Impulse);
+        }
+        if(rb.position.x <= 0)
+        {
+            rb.AddForce(Vector3.right, ForceMode.Impulse);
+        }     
+
+    }    
+}
